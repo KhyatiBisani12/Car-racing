8cc8ad6ef07019250eaf6a1efdefed883574450a
KhyatiBisani12 <80102728+KhyatiBisani12@users.noreply.github.com>
2021-05-18 17:02:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,32 @@
-var ball;
+
 var database;
-var position;
-var ballPosition;
+var gameState = 0;
+var playerCount = 0;
+var form;
+var game;
+var player;
+
 
 function setup(){
     // Binding your database and your code. Binding is done with database variable.
     database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
     
-    ballPosition = database.ref('ball/position'); // .ref() is used to point to a node in the database.
-    ballPosition.on("value",readPosition,showError);
+    game = new Game();
+    game.getState();
+    game.start();
+    
+    
+    /*ballPosition = database.ref('ball/position'); // .ref() is used to point to a node in the database.
+    ballPosition.on("value",readPosition,showError);*/
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
+    
 }
 
-function readPosition(data){
+/*function readPosition(data){
     position = data.val(); // ball/position
     ball.x = position.x;
     ball.y = position.y;
@@ -46,7 +41,4 @@ function writePosition(x,y){
     })
     
 }
-
-function showError(){
-    console.log("ERROR");
-}
+*/
